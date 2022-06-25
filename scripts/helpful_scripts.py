65090078b65f0e80bfcf7af9622aae49105e8267
ayushtues <ayushtues@gmail.com>
2022-06-25 15:08:04 +0530

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,33 @@
+from brownie import (
+    network,
+    config,
+    accounts,
+)
+from web3 import Web3
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = [
+    "development",
+    "ganache-local",
+    "mainnet-fork",
+    "ganache",
+    "hardhat",
+]
+
+DECIMALS = 8
+STARTING_PRICE = 200000000
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+
+    return accounts.add(config["wallets"]["from_key"])
